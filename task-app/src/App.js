29468ba2f8a6b6c11714d3660f8012eb427a9d9e
Ian Hess <ian.h.hess@gmail.com>
2023-03-08 15:26:27 -0500

    add button still need to figure out deleting

--- task-app/src/App.js
+++ task-app/src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
     super();
 
     this.state = {
-      task: { text: "" },
+      task: { text: "", id: uniqid() },
       tasks: [],
     };
   }
@@ -16,6 +16,7 @@ class App extends Component {
     this.setState({
       task: {
         text: e.target.value,
+        id: this.state.task.id,
       },
     });
   };
@@ -24,7 +25,7 @@ class App extends Component {
     e.preventDefault();
     this.setState({
       tasks: this.state.tasks.concat(this.state.task),
-      task: { text: "" },
+      task: { text: "", id: uniqid() },
     });
   };
 
